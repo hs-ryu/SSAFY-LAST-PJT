c3bcb2d17d1899faa6d06eeb91f4cdb15f82b910
gyoforit <gyoforit@gmail.com>
2021-05-21 01:44:41 +0900

    은교: 플랫폼 로고 이미지 추가 및 MovieDetail 플랫폼 로고 조건부 렌더링 구현

--- vue-client/src/components/MovieDetail.vue
+++ vue-client/src/components/MovieDetail.vue
@@ -3,6 +3,11 @@
     <img :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" :alt="movie.title">
     {{ movie.title }}
     {{ movie.content }}
+    <p>제공하는 플랫폼</p>
+    <a v-if="netflix" :href="netflix"><img src="@/assets/netflix_logo.png" alt="netflix logo"></a>
+    <a v-if="watcha" :href="watcha"><img src="@/assets/watcha_logo.png" alt="watcha logo"></a>
+    <a v-if="wavve" :href="wavve"><img src="@/assets/wavve_logo.png" alt="wavve logo"></a>
+    <a v-if="naver" :href="naver"><img src="@/assets/naver_logo.png" alt="naver logo"></a>
     <!-- {{ movie }} -->
   </div>
 </template>
@@ -16,6 +21,10 @@ export default {
   data: function () {
     return {
       movie: {},
+      netflix: '',
+      watcha: '',
+      wavve: '',
+      naver: '',
     }
   },
   methods: {
@@ -28,6 +37,10 @@ export default {
       })
       .then((res) => {
         this.movie = res.data
+        this.netflix = res.data.netflix
+        this.watcha = res.data.watcha
+        this.wavve = res.data.wavve
+        this.naver = res.data.naver
       })
       .catch((err) => {
         console.log(err)
